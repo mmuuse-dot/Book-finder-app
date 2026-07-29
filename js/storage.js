20b5c0ca55b41b43f7a2a906cd9b21e0361a08e5
mmuuse-dot <m.muuse@alustudent.com>
2026-07-29 10:34:40 -0700

    storage.js

---
+++ js/storage.js
@@ -0,0 +1,21 @@
+function saveFavorite(book) {
+
+    let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
+    const exists = favorites.some(item => item.title === book.title);
+
+    if (!exists) {
+
+        favorites.push(book);
+
+        localStorage.setItem("favorites", JSON.stringify(favorites));
+
+        alert("Book added to favorites!");
+
+    } else {
+
+        alert("This book is already in your favorites.");
+
+    }
+
+}
